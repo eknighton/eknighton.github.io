6e9542a2e3eab1861c76e6ee3998f4790ee55c26
eknighton <eknighton@me.com>
2023-05-08 02:40:32 -0700

    Tweaked display

--- levelOne.js
+++ levelOne.js
@@ -11,7 +11,7 @@
 
 //Critical Variables
 	let cardListIndex = 0;
-	let cardList = [{answer: true, question: 'wassup'}];
+	let cardList = [{answer: true, question: '1 + 1 = 2'},{answer: false, question: 'The Great Wall of China is visible from space with the naked eye.'},{answer: false, question: 'Albert Einstein was awarded the Nobel Prize in Physics for his work on the theory of relativity.'}];
 
 	let canvasX = 0;
 	let canvasY = 0;
@@ -19,17 +19,19 @@
 	let rightNum = NaN;
 	let leftNum = NaN;
 
+	let gameOver = false;
+
 //Canvas Variables
 	const canvas = document.getElementById('myCanvas');
 	const ctx = canvas.getContext('2d');
 	const MARGIN = 80;
 
 //Set Health
-	let bossHealth = 200;
-	let bossMaxHealth = 200;
+	let bossHealth = 100;
+	let bossMaxHealth = 400;
 
-	let playerHealth = 200;
-	let playerMaxHealth = 200;
+	let playerHealth = 400;
+	let playerMaxHealth = 400;
 
 //Ensures levelOne is only div displayed.
 	startScreen.style.display = 'none';
@@ -45,30 +47,78 @@
 //Overide slider inputs
 
     mouseUp = function() { // Uses left/right reward
-    	if (cardList[cardListIndex].answer == true){ // Checks answer, determining which side's reward is used.
-    		if (leftNum > 0){ //Checks sign of reward, deciding whether to deal or take damage.
-    			bossHealth  = bossHealth - leftNum;
-    		} else {
-    			playerHealth = playerHealth + rightNum;
-    		}
-    	} else {
-    		if (rightNum > 0){ //Checks sign of reward, deciding whether to deal or take damage.
-    			bossHealth = bossHealth - rightNum;
-    		} else {
-    			playerHealth = playerHealth + leftNum;
-    		}
-    	}
-    	document.getElementById('bossHP').innerText = "Boss Health: " + bossHealth + " / " + bossMaxHealth;
-    	document.getElementById('playerHP').innerText = "Player Health: " + playerHealth + " / " + playerMaxHealth;
-    	cardListIndex = cardListIndex + 1;
+    	//Modify and update player, boss health.
+	    	if (cardList[cardListIndex].answer == true){ // Checks answer, determining which side's reward is used.
+	    		if (leftNum > 0){ //Checks sign of reward, deciding whether to deal or take damage.
+	    			bossHealth  = bossHealth - leftNum;
+	    		} else {
+	    			playerHealth = playerHealth + leftNum;
+	    		}
+	    	} else {
+	    		if (rightNum > 0){ //Checks sign of reward, deciding whether to deal or take damage.
+	    			bossHealth = bossHealth - rightNum;
+	    		} else {
+	    			playerHealth = playerHealth + rightNum;
+	    		}
+	    	}
+	    	document.getElementById('bossHP').innerText = "Boss Health: " + bossHealth + " / " + bossMaxHealth;
+	    	document.getElementById('playerHP').innerText = "Player Health: " + playerHealth + " / " + playerMaxHealth;
+    	//Increase index, display next question.
+    		cardListIndex = cardListIndex + 1;
+    		updateCanvas();
+		//Check if game is over, display appropriate screen if so.
+	    	if (bossHealth < 0) {
+	    		gameOver = true;
+	    		winScreen();
+	    	}
+	    	if (playerHealth < 0) {
+	    		gameOver = true;
+	    		winScreen();
+	    	}
+	    	if (cardListIndex > 2) {
+	    		gameOver = true;
+	    		winScreen();
+	    	}
+
+
     };
     touchUp = mouseUp;
 
 
-//Update - for use by move triggers
-	function updateCanvas(){
+//Game Over Screen
+	function winScreen() {
 		// Clears entire canvas
 			ctx.clearRect(0, 0, canvas.width, canvas.height);
+		//
+		if (bossHealth < 0) {
+			ctx.fillStyle = 'green';
+			ctx.font = '40px Arial';
+			let bossTextWidth = ctx.measureText('You defeated the boss!').width;
+			ctx.fillText('You defeated the boss!', (canvas.width - bossTextWidth) / 2 , canvas.height/2);
+		} else if (playerHealth < 0){
+			ctx.fillStyle = 'red';
+			ctx.font = '40px Arial';
+			let bossTextWidth = ctx.measureText('You died.').width;
+			ctx.fillText('You died.', (canvas.width - bossTextWidth) / 2 , canvas.height/2);
+		} else {
+			ctx.fillStyle = 'red';
+			ctx.font = '40px Arial';
+			let bossTextWidth = ctx.measureText('You ran out of turns.').width;
+			ctx.fillText('You ran out of turns.', (canvas.width - bossTextWidth) / 2 , canvas.height/2);
+		}
+	}
+
+
+//Update - for use by move triggers
+	function updateCanvas(){
+
+		// Check if Game Over
+
+			if (gameOver) {
+				winScreen();
+				return;
+			}
+
 		// Convert mouse position to canvas position
 			 const canvasRect = canvas.getBoundingClientRect();
 			 const scaleX = (canvas.width - 2 * MARGIN) / canvasRect.width;
@@ -78,6 +128,9 @@
 			 canvasY = (mouseCurrentY - canvasRect.top) * scaleY;
 			 canvasX = Math.max(MARGIN, Math.min(canvasX, canvas.width - MARGIN));
 			 console.log("Canvas X " + canvasX)
+
+		// Clears entire canvas
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
 		//Fill background
 			ctx.fillStyle = 'green';
 			ctx.fillRect(0, 0, canvasX, canvas.height);
@@ -90,7 +143,6 @@
 			ctx.moveTo(lineX, 0);
 			ctx.lineTo(lineX, canvas.height);
 			ctx.stroke();
-
 		// Draw the words "True" and "False"
 			const textY = 30;
 			ctx.font = '20px Arial';
@@ -120,16 +172,24 @@
 				intercept = radius - Math.sqrt(-valueInsideSqrt) - Math.abs(slope * wager);
 			}
 			console.log("intercept " + intercept);
-			leftNum = Math.floor(intercept+radius*slope);
-			rightNum = Math.floor(intercept-radius*slope);
+			leftNum = Math.floor(intercept+radius*slope)-205; // Why tf didd I have to subtract 205?
+			rightNum = Math.floor(intercept-radius*slope)-205; // Also, sccratch got different numbers, def try plugging in to checck w grapher
 
-		// Draw the number "5"
+		// Draw the numbers
 			const numberY = canvas.height / 2;
 			ctx.font = '20px Arial';
 			const leftNumberWidth = ctx.measureText(leftNum).width;
 			const rightNumberWidth = ctx.measureText(rightNum).width;
 			ctx.fillText(leftNum, (lineX - leftNumberWidth) / 2, numberY);
 			ctx.fillText(rightNum, (lineX - rightNumberWidth + canvas.width) / 2, numberY);
+
+		// Ask the question
+			const questionY = canvas.height /4;
+			ctx.fillStyle = 'white';
+			ctx.font = '20px Arial';
+			let questionWidth = ctx.measureText(cardList[cardListIndex].question).width;
+			ctx.fillText(cardList[cardListIndex].question, (canvas.width - questionWidth) / 2 , questionY);
+			ctx.fillStyle = 'black';
 	}
 
 
