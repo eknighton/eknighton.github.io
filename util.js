a7a0ff42de94fcfbfa841bce728f4a2636f62c04
eknighton <eknighton@me.com>
2023-05-08 12:34:26 -0700

    added shitty wrap text to util

--- util.js
+++ util.js
@@ -14,3 +14,23 @@
 
       return Array.from(numbers);
     }
+
+//Wrap text... duh
+   function wrapText(ctx, text, x, y, maxWidth, lineHeight) {
+        const words = text.split(' ');
+        let line = '';
+
+        for (let n = 0; n < words.length; n++) {
+          const testLine = line + words[n] + ' ';
+          const metrics = ctx.measureText(testLine);
+          const testWidth = metrics.width;
+          if (testWidth > maxWidth && n > 0) {
+            ctx.fillText(line, x, y);
+            line = words[n] + ' ';
+            y += lineHeight;
+          } else {
+            line = testLine;
+          }
+        }
+        ctx.fillText(line, x, y);
+      }
