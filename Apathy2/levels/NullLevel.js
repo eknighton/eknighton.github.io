4effb039a5ee0c764cde07016f473d0cb180662d
eknighton <eknighton@me.com>
2024-03-04 16:40:40 -0800

    Added apathy2

---
+++ Apathy2/levels/NullLevel.js
@@ -0,0 +1,3 @@
+data['NullLevel'] = {
+	'puzzle': 'kkkkkkkk/kkkkkkkk/kkkkkkkk/kkkkkkkk/KKKKKKKK/KKKKKKKK/KKKKKKKK/KKKKKKKK w - - 0 1'
+};
---
+++ Apathy2/loader.js
@@ -0,0 +1,20 @@
+var data = [];
+const scripts = ['NullLevel.js','2RooksMate.js', 'LonelyKing.js', 'ArabianKnights.js','KnightsTemplar.js','FiveCompanions.js']; // List of scripts to load
+
+const scriptPromises = scripts.map(script => {
+    return new Promise((resolve, reject) => {
+        const scriptTag = document.createElement('script');
+        scriptTag.src = `levels/${script}`;
+        scriptTag.onload = resolve; // Resolve the promise when the script is loaded
+        scriptTag.onerror = reject; // Reject the promise if there's an error loading the script
+        document.body.appendChild(scriptTag);
+    });
+});
+
+Promise.all(scriptPromises).then(() => {
+    console.log('All dependent scripts have loaded.');
+    dataLoaded()
+    // Now it's safe to execute code that depends on these scripts
+}).catch(error => {
+    console.error('A script failed to load:', error);
+});
