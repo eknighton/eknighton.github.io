ca0a371b313303e79420c8f8935e4e424b875a3c
eknighton <eknighton@me.com>
2024-04-25 10:48:00 -0700

    Began organizing items

--- Today/script.js
+++ Today/script.js
@@ -13,7 +13,61 @@ document.addEventListener("DOMContentLoaded", function() {
         "What memories did you revisted today?",
         "Did you meet anyone new today? What were they like?",
         "Did you try something new today?",
-        "How did you feel when you woke up?"
+        "What's something difficult you did today?",
+        "What's a mistake you made today?",
+        "What's something selfish you did today?",
+        "What did you do today that you are proud of?",
+        "When did you surprise yourself today?",
+        "What was it like outside today?",
+        "What's a problem you solved today?",
+        "What kind of person were you today?",
+        "What's a disagreement you had today?",
+        "What couldn't you figure out today?",
+        "What information did you seek out today?",
+        "How did you spend your time today?",
+        "What did you put off doing today?",
+        "What did you plan to do today, but didn't?",
+        "Was there anything today you told yourself you'd do next time?",
+        "What was something beautiful you saw today?",
+        "What's something you imagined today?",
+        "What's something you fantasized about today?",
+        "What's something you imagined having that you don't have?",
+        "Today, was there anything you considered but decided wasn't worth doing?",
+        "Did you talk to any friends today?",
+        "What wasted your time today?",
+        "What's something unusual you did today?",
+        "What's something weird you did today?",
+        "What's something possibly unethical you did today?",
+        "What's something you questioned today?",
+        "What's something you experimented with today?",
+        "What's something you practiced today?",
+        "What's something you studied today?",
+        "What's something you made today?",
+        "What's somethign you designed today?",
+        "What's something you fixed today?",
+
+        //Acheivement Pack
+
+        //People Pack
+        "What people did you see today?",
+
+        //Learning Pack
+
+        //Feelings Pack
+        "What made you laugh today?",
+        "When were you inspired today?",
+
+        //Family Pack
+        "What did the people you care about do today?",
+        "What brought your loved ones joy today?",
+
+        //Nature Pack
+        "What animals did you see today?",
+        "What bugs did you see today?",
+        "What clouds did you see today?"
+
+        //We can use a 2D array with followup questions?
+        //We create question packs
 
     ];
 
