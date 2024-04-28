51a9fbd1b8abbd0a57226a7d44fa7da0b1dd1c1e
eknighton <eknighton@me.com>
2024-04-28 01:36:11 -0700

    Timer Bugfix

--- Text Adventure/util.js
+++ Text Adventure/util.js
@@ -1,13 +1,21 @@
 function startCountdown(seconds, elementId, callback) {
-    let countdownElement = null;
+    let countdownElement = document.getElementById(elementId) || null;
+    if (countdownElement){
+        countdownElement.textContent = seconds+"s";
+    }
+
+    //!!! Chance that this setting will not succeed! 
 
     const intervalId = setInterval(() => {
         seconds--;  // Decrease the seconds
         countdownElement = document.getElementById(elementId);
-        if (seconds <= 0 || !countdownElement) {
-            clearInterval(intervalId);  // Stop the countdown
+        if (!countdownElement) {
+            clearInterval(intervalId);
+        } else if (seconds <= 0){
             callback();
+            clearInterval(intervalId);
         }
+
         countdownElement.textContent = seconds+"s";  // Update the display
     }, 1000);  // Update every 1000 milliseconds (1 second)
 }
