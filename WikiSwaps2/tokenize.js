b24a7284283644081d803ee58f5d9e2a4f6c74d2
eknighton <eknighton@me.com>
2024-12-18 12:50:43 -0800

    Update tokenize.js

--- WikiSwaps2/tokenize.js
+++ WikiSwaps2/tokenize.js
@@ -5,16 +5,26 @@ function tokenize(text) {
 }
 
 function splitIntoSentences(text) {
+    // Make sure newlines are consistent and without duplicates.
+    smoothedText = text.replace(/(\r\n|\n|\r)/gm, "\n");
+    smoothedText = smoothedText.replace("\n\n\n", "\n\n");
     // Regular expression that splits the text at each period, question mark, or exclamation mark followed by a space or end of text.
-    const sentenceRegex = /(?<!\b(?:Mr|Mrs|Ms|Dr|Jr|Sr|vs)\.)(?<!\b\w\.\w\.)(?<=[.?!])\s+(?=[A-Z])/;
-    return text.split(sentenceRegex);
+    const sentenceRegex = /(?<!\b(?:Mr|Mrs|Ms|Dr|Jr|Sr|vs)\.)(?<!\b\w\.\w\.)(?<=[.?!])(?:\s|\n|\n\n)/;
+    return smoothedText.split(sentenceRegex);
 }
 
 function groupSentencesIntoBlocks(sentences) {
     const blocks = [];
     for (let i = 0; i < sentences.length; i += 2) {
-        // Combine two sentences or include the last single sentence if it's the last one without a pair.
-        const block = sentences[i] + (sentences[i + 1] ? ' ' + sentences[i + 1] : '');
+        // The last sentence of a section or of the article should be its own block if necessary.
+        if (sentences[i + 1].includes("==") || !sentences[i + 1]) {
+            const block = sentences[i];
+            blocks.push(block);
+            i--;
+            continue;
+        }
+        // Combine two sentences.
+        const block = sentences[i] + ' ' + sentences[i + 1];
         blocks.push(block);
     }
     return blocks;
